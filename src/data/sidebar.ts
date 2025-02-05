26452c926acc1f6318b207c43aba6ca7f985e624
Neil Miller <neildmiller@gmail.com>
2025-02-05 09:15:02 -0600

    App Sidebar

---
+++ src/data/sidebar.ts
@@ -0,0 +1,10 @@
+export const sidebarComponents = {
+    title: 'Components',
+    items: [
+        { title: 'Button', href: '/docs/components/button' },
+        { title: 'Card', href: '/docs/components/card' },
+        { title: 'Header Page', href: '/docs/components/header-page' }, 
+        { title: 'Header Section', href: '/docs/components/header-section' },
+        { title: 'Sidebar', href: '/docs/components/sidebar' },
+    ]
+}
