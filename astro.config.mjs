6de47142b9f065326bce6e60fddc6d9834614c7e
Neil Miller <neildmiller@gmail.com>
2025-03-30 21:25:02 -0500

    Upgrade Astro, Tailwind, All

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [mdx()],
+
   redirects: {
     '/docs/components': '/docs/components/button'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
