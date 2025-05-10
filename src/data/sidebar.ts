6572f3fdfc2d103873996c9a21e78d03c5bb3ee2
Neil Miller <neildmiller@gmail.com>
2025-05-10 08:54:50 -0500

    Layout Updates

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -1,10 +1,13 @@
 export const sidebarComponents = {
     title: 'Components',
     items: [
+        { title: 'Alert', href: '/docs/components/alert' },
         { title: 'Button', href: '/docs/components/button' },
         { title: 'Card', href: '/docs/components/card' },
+        { title: 'Container', href: '/docs/components/container' },
         { title: 'Header Page', href: '/docs/components/header-page' }, 
         { title: 'Header Section', href: '/docs/components/header-section' },
+        { title: 'Metric', href: '/docs/components/metric' },
         { title: 'Sidebar', href: '/docs/components/sidebar' },
     ]
 }
@@ -14,18 +17,22 @@ export const sidebarLinks = [
         title: 'Getting Started',
         items: [
             { title: 'Installation', href: '/docs/installation' },
+            { title: 'Design Cues', href: '/docs/design-cues' },
             { title: 'Typography', href: '/docs/typography' },
         ]
     },
     {
         title: 'Components',
         items: [
+            { title: 'Alert', href: '/docs/components/alert' },
             { title: 'Badge', href: '/docs/components/badge' },
             { title: 'Breadcrumb', href: '/docs/components/breadcrumb' },
             { title: 'Button', href: '/docs/components/button' },
             { title: 'Card', href: '/docs/components/card' },
+            { title: 'Container', href: '/docs/components/container' },
             { title: 'Header Page', href: '/docs/components/header-page' }, 
             { title: 'Header Section', href: '/docs/components/header-section' },
+            { title: 'Metric', href: '/docs/components/metric' },
             { title: 'Sidebar', href: '/docs/components/sidebar' },
             { title: 'Tablist', href: '/docs/components/tablist' },
             { title: 'Vertlist', href: '/docs/components/vertlist' },
